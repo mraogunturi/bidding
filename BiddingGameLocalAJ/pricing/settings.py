bfdce9a10ecdc5d6141ccd2adca1bd1832af6936
mgunturx <maheswara.raox.gunturi@intel.com>
2021-02-12 16:26:49 -0700

    frameworks

--- BiddingGameLocalAJ/pricing/settings.py
+++ BiddingGameLocalAJ/pricing/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # pricing simulation an below are apps
     'pricingsimulation',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
